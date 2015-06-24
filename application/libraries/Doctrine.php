289f0c73c4068c36b384989acfc28de786328e21
santhapa <san.loggingin@gmail.com>
2015-06-24 13:08:33 +0545

    commenting sql logger

--- application/libraries/Doctrine.php
+++ application/libraries/Doctrine.php
@@ -80,8 +80,8 @@ class Doctrine {
         $config->setProxyNamespace('Proxies');
 
         // Set up logger
-        $logger = new EchoSQLLogger;
-        $config->setSQLLogger($logger);
+        // $logger = new EchoSQLLogger;
+        // $config->setSQLLogger($logger);
 
         $config->setAutoGenerateProxyClasses( TRUE );
 
